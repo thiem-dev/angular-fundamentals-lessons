4caa59062c202d4075608e5a6ebfcaac871b5653
Thiem <thiem.huynh0@gmail.com>
2024-03-24 18:17:57 -0400

    basic angular component

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,16 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Fav Song</li>
+      <li>Yesman</li>
+      <li>3rd item</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+    ol{
+      list-style-type: upper-roman;
+    }
+  `,
 })
 export class AppComponent {}
