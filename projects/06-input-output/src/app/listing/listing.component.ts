abb2fdf08022b4df8626f33fb8839d807fcbc94c
Thiem Huynh <thiem.huynh0@gmail.com>
2024-03-27 12:35:29 -0400

    06 lesson update

--- projects/06-input-output/src/app/listing/listing.component.ts
+++ projects/06-input-output/src/app/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Car } from '../car';
 
@@ -6,7 +6,35 @@ import { Car } from '../car';
   selector: 'app-listing',
   standalone: true,
   imports: [CommonModule],
-  template: ` <!-- listing markup goes here --> `,
+  template: `<!-- This article element represents and entire listing -->
+    <article class="listing">
+      <div class="image-parent">
+        <img class="product-image" src="https://placehold.co/100x100" />
+      </div>
+      <section class="details">
+        <p class="title"><!-- car make and model--></p>
+        <hr />
+        <p class="detail">
+          <span>Year</span>
+          <span><!-- year --></span>
+        </p>
+        <div class="detail">
+          <span>Transmission</span>
+          <span><!-- transmission --></span>
+        </div>
+        <p class="detail">
+          <span>Mileage</span>
+          <span><!-- miles --></span>
+        </p>
+        <p class="detail">
+          <span>Price</span>
+          <span><!-- price --></span>
+        </p>
+      </section>
+    </article>
+    <!-- end car listing markup -->`,
   styles: ``,
 })
-export class ListingComponent {}
+export class ListingComponent {
+  @Input({ required: true }) car!: Car;
+}
