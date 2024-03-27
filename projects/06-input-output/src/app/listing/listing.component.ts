b28a30da9813f2872abfe914e388f767ff060e63
Thiem Huynh <thiem.huynh0@gmail.com>
2024-03-27 12:36:45 -0400

    06 lesson update

--- projects/06-input-output/src/app/listing/listing.component.ts
+++ projects/06-input-output/src/app/listing/listing.component.ts
@@ -12,23 +12,23 @@ import { Car } from '../car';
         <img class="product-image" src="https://placehold.co/100x100" />
       </div>
       <section class="details">
-        <p class="title"><!-- car make and model--></p>
+        <p class="title">{{ car.model }} {{ car.make }}</p>
         <hr />
         <p class="detail">
           <span>Year</span>
-          <span><!-- year --></span>
+          <span>{{ car.year }}</span>
         </p>
         <div class="detail">
           <span>Transmission</span>
-          <span><!-- transmission --></span>
+          <span>{{ car.transmission }}</span>
         </div>
         <p class="detail">
           <span>Mileage</span>
-          <span><!-- miles --></span>
+          <span>{{ car.miles }}</span>
         </p>
         <p class="detail">
           <span>Price</span>
-          <span><!-- price --></span>
+          <span>{{ car.price }}</span>
         </p>
       </section>
     </article>
